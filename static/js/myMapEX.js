067f04c5e5949180b645f68b9f4f828e1f138b13
Fong-Xuan <fong1143@gmail.com>
2017-01-06 00:08:36 +0800

    Add files via upload

--- static/js/myMapEX.js
+++ static/js/myMapEX.js
@@ -1,22 +1,22 @@
 function mouseInPhoto(){
 	var icon = this.icon;
-	icon.size.height *= 1.5;
-	icon.size.width *= 1.5;
+	icon.size.height = 90;
+	icon.size.width = 120;
 	this.setIcon(icon);
 	
 }
 function mouseOutPhoto(){
 	var icon = this.icon;
-	icon.size.height /= 1.5;
-	icon.size.width /= 1.5;
+	icon.size.height = 60;
+	icon.size.width = 80;
 	this.setIcon(icon);
 }
 function dblclickOnPhoto(){
 	this.setDraggable(!this.draggable);
 	if(!this.draggable){
 		var icon = this.icon;
-		icon.size.height /= 1.5;
-		icon.size.width /= 1.5;
+		icon.size.height = 60;
+		icon.size.width = 80;
 		this.setIcon(icon);
 		this.setOptions({'opacity': 1})
 	} else {
