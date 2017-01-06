592ca3afa9be64df179f10abc393c95775485041
andykao1213 <s103062209@m103.nthu.edu.tw>
2017-01-06 11:10:16 +0800

    Add files via upload

--- yo.py
+++ yo.py
@@ -14,4 +14,6 @@ def index():
 
 #Default value of name is None (null)
 if __name__ == '__main__':
-    app.run(host='0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0',debug=True, port=5000)
+
+#fuck you
